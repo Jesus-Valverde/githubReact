940fc7dcb2e407327a6274399faf509ccbb9cc53
Jesus Valverde <jesusvalverde.dev@outlook.com>
2025-04-02 22:50:58 -0700

    Agregado la lista de datos de las personas, nueva función para la tabla MUI y tabla que muestra los datos de las personas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Contador from '../contador' 
+import * as React from 'react';
+
+// Componentes de MUI
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
 
 function App() {
+
+  // Función por parte de MUI para agregar los datos a la tabla
+  function createData(nombre, apellido, edad) {
+    return { nombre, apellido, edad};
+  }
+
+  // Arreglo con los datos de las personas
+  const personas = [
+    { nombre: "Angel", apellido: "Ruiz", edad: 22 },
+    { nombre: "Ariel", apellido: "Sotomayor", edad: 21 },
+    { nombre: "David", apellido: "Santana", edad: 21 },
+    { nombre: "Antonio", apellido: "Medina", edad: 21 },
+    { nombre: "Javier", apellido: "Granados", edad: 21 },
+    { nombre: "Danna", apellido: "Butchart", edad: 22 }
+  ];
+
+  // Transformar los datos en un formato para la tabla
+  const rows = personas.map(persona => createData(persona.nombre, persona.apellido, persona.edad));
 
   return (
     <>
@@ -22,6 +51,27 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        {/* Tabla generada a partir de MUI */}
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Nombre</TableCell>
+                <TableCell>Apellido</TableCell>
+                <TableCell>Edad</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row, index) => (
+                <TableRow key={index}>
+                  <TableCell>{row.nombre}</TableCell>
+                  <TableCell>{row.apellido}</TableCell>
+                  <TableCell>{row.edad}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
